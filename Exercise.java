480fe9212868506339c8ac4a0b8ca40057a9e22d
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2024-02-13 19:26:39 -0500

    Init commit

---
+++ Exercise.java
@@ -0,0 +1,11 @@
+public class Exercise {
+  /**
+   * Which muscle group this exercise targets.
+   */
+  MuscleGroup muscleGroup;
+
+  /**
+   * The historic max weight the user was able to lift with this exercise.
+   */
+  double personalRecordWeight;
+}
---
+++ MuscleGroup.java
@@ -0,0 +1,11 @@
+/**
+ * Denotes the 13 major muscle groups of the human body.
+ */
+public enum MuscleGroup{
+  QUADS, CALVES, HAMSTRING, GLUTES,
+  BICEPS, TRICEPS, FOREARMS, TRAPS,
+
+  LATS, DELTS, BACK,
+
+  ABS, OBLIQUES
+}
