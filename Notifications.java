423cb88c8517c9e4256445c6dbad8233bcc6bd68
RTL-14 <163361045+RTL-14@users.noreply.github.com>
2024-04-08 16:05:10 -0400

    Update Notifications.java

--- Notifications.java
+++ Notifications.java
@@ -1,24 +1,55 @@
+/* 
+ * IMPORTANT NOTE: this should send daily notifications starting at the time and date they are turned on,
+ * however I am still testing to see if it actually works, but as of now it does send a notif when you run it,
+ * so it could probably be good enough for a demo.
+ */
+
 import java.awt.*;
 import java.awt.TrayIcon.MessageType;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Date;
 
 /*
  * As I was unfamiliar with how to send notifications in java, I followed advice and demos
- * on stackoverflow to get this working.
- * INCOMPLETE AS OF NOW, STILL NEED TO FIGURE OUT SCHEDULING THE NOTIFS
+ * on stackoverflow to get this working
  */
 
 public class Notifications {
 	
+	Date date;
+	boolean notifOn = true;
+	
 	public Notifications() {}
 	
-	public static void main(String[] args) throws AWTException{
-		if(SystemTray.isSupported()) {
-			Notifications notif = new Notifications();
-			notif.notifyApp();
-		}
-		else {
-			System.err.println("Not supported");
-		}
+	public static void main(String[] args) {
+		Notifications notif = new Notifications();
+		notif.notif();
+	}
+	
+	public void notif() {
+		TimerTask task = new TimerTask() {
+			public void run() {
+				if(SystemTray.isSupported()) {
+					Notifications notif = new Notifications();
+					try {
+						notif.notifyApp();
+					} catch (AWTException e) {
+						e.printStackTrace();
+					}
+				}
+				else {
+					System.err.println("Not supported");
+				}
+			}
+		};
+		Timer timer = new Timer("Timer");
+		long period = 1000L * 60L * 60L * 24L;
+		this.setDate();
+	    timer.scheduleAtFixedRate(task, date, period);
+	    if(!notifOn) {
+	    	timer.cancel();
+	    }
 	}
 	
 	public void notifyApp() throws AWTException{
@@ -34,5 +65,13 @@ public class Notifications {
 		ti.displayMessage("Remember to workout today!",
 				"Keep up the good work, you've got this!",
 				MessageType.INFO);
+	}
+	
+	private void setDate() {
+		date = new Date();
+	}
+	
+	public void notifOff() {
+		this.notifOn = false;
 	}
 }
